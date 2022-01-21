41a49dea18853b5e208db975bd49883790c42f10
AdrianMiernik <adik.miernik@gmail.com>
2022-01-21 12:02:21 +0100

    Student: service - methods implementation.

--- src/main/java/pl/miernik/university/students/StudentService.java
+++ src/main/java/pl/miernik/university/students/StudentService.java
@@ -2,9 +2,13 @@ package pl.miernik.university.students;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -13,26 +17,31 @@ public class StudentService implements IStudentService {
 
     @Override
     public void saveStudent(Student student) {
-
+        studentRepository.save(student);
     }
 
     @Override
     public Student getStudentById(Integer id) {
-        return null;
+        Optional<Student> optional = studentRepository.findById(id);
+        return optional.orElse(null);
     }
 
     @Override
     public void deleteStudent(Integer id) {
-
+        studentRepository.deleteById(id);
     }
 
     @Override
     public List<Student> studentList() {
-        return null;
+        return studentRepository.findAll();
     }
 
     @Override
     public Page<Student> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
-        return null;
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+        return studentRepository.findAll(pageable);
     }
 }
